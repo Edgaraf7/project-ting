227a32d85dae3a89edc1122930c3e96d2da636a3
edgar <edgar.dev22@gmail.com>
2024-04-01 18:40:03 -0300

    part1

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,16 +1,16 @@
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 import os
-from queue import Queue
-from file_management import txt_importer
 
 
-def process(queue, file_path):
+def process(path_file: str, instance: Queue):
     # Verificar se o arquivo já foi processado anteriormente
-    file_name = os.path.basename(file_path)
-    if file_name in queue.processed_files:
+    file_name = os.path.basename(path_file)
+    if instance.is_processed(file_name):
         return
 
     # Ler as linhas do arquivo usando txt_importer
-    lines = txt_importer(file_path)
+    lines = txt_importer(path_file)
 
     # Verificar se as linhas foram lidas com sucesso
     if lines is None:
@@ -18,27 +18,26 @@ def process(queue, file_path):
 
     # Construir o dicionário com os dados processados
     processed_data = {
-        "nome_do_arquivo": file_name,
+        "nome_do_arquivo": path_file,
         "qtd_linhas": len(lines),
         "linhas_do_arquivo": lines
     }
 
     # Adicionar o arquivo à fila
-    queue.enqueue(processed_data)
+    instance.enqueue(processed_data)
 
     # Mostrar os dados processados via stdout
     print(processed_data)
 
 
-if __name__ == "__main__":
-    queue = Queue()  # Crie uma instância da fila
-    file_path = "exemplo.txt"  # Caminho do arquivo
-    process(queue, file_path)
+def remove(instance: Queue, file_name: str):
+    instance.items = [item for item in
+                      instance.items if item["nome_do_arquivo"] != file_name]
+    print(f"Arquivo {file_name} removido com sucesso")
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
-
-
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+def file_metadata(instance: Queue, position):
+    try:
+        print(instance.search(position))
+    except IndexError:
+        print("Posição inválida")
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,12 +4,15 @@ from ting_file_management.abstract_queue import AbstractQueue
 class Queue(AbstractQueue):
     def __init__(self):
         self.items = []
+        self.processed_files = set()
 
     def __len__(self):
         return len(self.items)
 
     def enqueue(self, value):
         self.items.append(value)
+        file_name = value.get("nome_do_arquivo")
+        self.processed_files.add(file_name)
 
     def dequeue(self):
         if len(self.items) == 0:
@@ -20,3 +23,6 @@ class Queue(AbstractQueue):
         if not 0 <= index < len(self.items):
             raise IndexError("Índice Inválido ou Inexistente")
         return self.items[index]
+
+    def is_processed(self, file_name):
+        return file_name in self.processed_files
