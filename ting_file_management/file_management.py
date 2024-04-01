ff2bdfceb47a5d0f7987718de23d25d2e2f64bd4
edgar <edgar.dev22@gmail.com>
2024-04-01 18:06:04 -0300

    file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(file_path):
+    if not file_path.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+        return None
+
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.read().split('\n')
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {file_path} não encontrado\n")
+        return None
+
+
+file_path = "exemplo.txt"
+lines = txt_importer(file_path)
+if lines is not None:
+    for line in lines:
+        print(line)
