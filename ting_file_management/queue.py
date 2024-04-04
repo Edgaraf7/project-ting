1d8766e595d9b306d428b0631e2033e3f22235d4
edgar <edgar.dev22@gmail.com>
2024-04-04 16:37:42 -0300

    ajustes

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,25 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-
         self.items = []
 
     def __len__(self):
-
         return len(self.items)
 
     def enqueue(self, value):
-
         self.items.append(value)
 
     def dequeue(self):
-
         if len(self.items) == 0:
             return None
         return self.items.pop(0)
 
     def search(self, index):
-
         if not 0 <= index < len(self.items):
             raise IndexError("Índice Inválido ou Inexistente")
         return self.items[index]
+
+    def is_empty(self):
+        return len(self.items) == 0
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance.items:
+        file_path = item["nome_do_arquivo"]
+        occurrences = []
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for idx, line in enumerate(lines, start=1):
+                if word.lower() in line.lower():
+                    occurrences.append({"linha": idx})
+        if occurrences:
+            result.append({"palavra": word,
+                           "arquivo": file_path, "ocorrencias": occurrences})
+    return result  #
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance.items:
+        file_path = item["nome_do_arquivo"]
+        occurrences = []
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for idx, line in enumerate(lines, start=1):
+                if word.lower() in line.lower():
+                    occurrences.append({"linha": idx,
+                                        "conteudo": line.strip()})
+        if occurrences:
+            result.append({"palavra": word,
+                           "arquivo": file_path, "ocorrencias": occurrences})
+    return result
